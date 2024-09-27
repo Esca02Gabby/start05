396e12dfd5dd89d5713693159cb4252f444704be
Gabby Escamilla <gabrielaescamilla255@yahoo.com>
2024-09-26 21:43:12 -0600

    first commit

---
+++ data/reviews.js
@@ -0,0 +1,22 @@
+export const reviews = [
+    {
+      "name": "Peter Parker",
+      "stars": 4,
+      "review": "Had a great experience at SpeedLane Autos! Found the perfect car for my needs. The staff was friendly and helpful throughout the entire process. Would definitely recommend!"
+    },
+    {
+      "name": "Bruce Wayne",
+      "stars": 2,
+      "review": "Not impressed with my experience at SpeedLane Autos. The car I purchased had more issues than I anticipated, and the customer service could have been better. Disappointed overall."
+    },
+    {
+      "name": "Tony Stark",
+      "stars": 5,
+      "review": "Absolutely thrilled with the service at SpeedLane Autos! From start to finish, the team went above and beyond to help me find the perfect vehicle. Smooth transaction, and I couldn't be happier with my new car!"
+    },
+    {
+      "name": "Maria Hill",
+      "stars": 3,
+      "review": "Mixed feelings about SpeedLane Autos. While the car selection was decent, the prices were a bit higher than I expected. However, the staff was knowledgeable and helped me find a car that met my requirements."
+    }
+  ]
